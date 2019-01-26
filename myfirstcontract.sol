e7c9ead2968df916edb662ede98b66de1ad4796d
jgyun <jgyun@kr.ibm.com>
2019-01-26 21:16:12 +0900

    Develop Interitance using solidity

--- myfirstcontract.sol
+++ myfirstcontract.sol
@@ -1,17 +1,56 @@
 pragma solidity ^0.4.24;
 
-contract MyFirstContract {
+interface Validator {
+    function checkValue(uint amount) external view returns (bool);
+    function loan() external returns (bool);
+}
+
+contract Bank is Validator {
+    uint private value = 10;
+    
+    constructor(uint amount) public {
+        value = amount;
+    }
+    
+    function deposit(uint amount) public {
+        value += amount;
+    }
+    
+    function withdraw(uint amount) public {
+        if (checkValue(amount))
+            value -= amount;
+    }
+    
+    function balance() public view returns (uint) {
+        return value;
+    }
+    
+    
+    function checkValue(uint amount) public view returns (bool) {
+        return amount <= value;
+    }
+    
+    //abstract function
+    //function load() public returns (bool);
+        
+    function loan() public view returns (bool) {
+        return value > 0;
+    }
+}
+
+contract MyFirstContract is Bank(20) {
     string private name;
     uint private age;
     
     function setName(string _name) public {
         name = _name;
+        
     }
     
     function getName()  public view returns  (string) {
         return name;
     }
-
+    
     function getAge() public view returns (uint) {
         return age;
     }
